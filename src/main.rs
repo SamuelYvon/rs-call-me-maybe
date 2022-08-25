3c35202ee2f0f5a03d33d41d49571c7c84dbbd28
Samuel Yvon <samuelyvon9@gmail.com>
2022-08-25 12:04:41 -0400

    Pushover client

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use std::io;
+use std::collections::HashMap;
 use Result::Err;
+
+const PUSH_OVER_URL : &str = "https://api.pushover.net/1/messages.json";
+
+fn pusherover(message : String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut json_data : HashMap<&str, String> = HashMap::new();
+
+    json_data.insert("token", "".to_string());
+    json_data.insert("user", "".to_string());
+    json_data.insert("message", message);
+
+    let client = reqwest::blocking::Client::new();
+    let _ = client.post(PUSH_OVER_URL)
+        .json(&json_data)
+        .send();
+
+
+    Ok(())
+}
 
 fn read_stdin() -> Vec<String> {
     let stdin = io::stdin();
@@ -23,7 +42,6 @@ fn read_stdin() -> Vec<String> {
 }
 
 fn main() {
-    for line in read_stdin().iter() {
-        print!("{line}");
-    }
+    let message = read_stdin().join("\n");
+    pusherover(message);
 }
