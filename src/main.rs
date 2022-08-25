ae13abfc0d13dc6d15539a55a536911118b60c6c
Samuel Yvon <samuelyvon9@gmail.com>
2022-08-25 11:27:09 -0400

    Read input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+use Result::Err;
+
+fn read_stdin() -> Vec<String> {
+    let stdin = io::stdin();
+    let mut lines : Vec<String> = Vec::new();
+
+    loop {
+        let mut line = String::new();
+
+        match stdin.read_line(&mut line) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+                lines.push(line);
+            },
+            Err(e) => panic!("{e}")
+        }
+    }
+
+    return lines;
+}
+
 fn main() {
-    println!("Hello, world!");
+    for line in read_stdin().iter() {
+        print!("{line}");
+    }
 }
